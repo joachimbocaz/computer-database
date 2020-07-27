8b53f0f5fcab770bac348b0b9436bb7d2f6d1562
joachimbocaz <joachimbocaz@gmail.com>
2020-07-27 16:13:59 +0200

    suppression ancien projet

--- computer-database/src/main/java/excilys/formation/java/cbd/customer/ComputerRepository.java
+++
@@ -1,9 +0,0 @@
-package excilys.formation.java.cbd.customer;
-
-import org.springframework.data.repository.CrudRepository;
-
-import excilys.formation.java.cbd.model.Computer;
-
-public interface ComputerRepository  extends CrudRepository<Computer, Long>{
-	
-}
--- computer-database/src/test/resources/2-PRIVILEGES.sql
+++
@@ -1,9 +0,0 @@
-  #-----------------------------------
-  #USER RIGHTS MANAGEMENT
-  #-----------------------------------
-  CREATE USER 'admincdb-test'@'localhost' IDENTIFIED BY 'qwerty1234';
-
-  GRANT ALL PRIVILEGES ON `computer-database-db-test`.* TO 'admincdb-test'@'localhost' WITH GRANT OPTION;
-
-
-  FLUSH PRIVILEGES;
